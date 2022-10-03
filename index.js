af126f93ad2b99f024bd1ba8d18d277d9e2f052c
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2022-10-03 11:46:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use('/', RegisterRoute);
 app.use('/user', RegisterRoute);
 
-const PORT =  8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
